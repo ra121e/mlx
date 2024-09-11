5de914a9a45bbe1bb91c1935dc54f1cd023c4c3c
root <sushi.honda@gmail.com>
2024-09-11 09:02:12 +0800

    paint color

--- draw.c
+++ draw.c
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 17:59:34 by athonda           #+#    #+#             */
-/*   Updated: 2024/09/09 22:11:54 by athonda          ###   ########.fr       */
+/*   Updated: 2024/09/11 08:46:21 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,16 +30,34 @@ void	draw(t_box *p)
 			p->cy = (y - p->orig_y) * (p->scale_factor * 4.0 / WIDTH);
 			p->x = x;
 			p->y = y;
-			if (fractal(p) < p->iter + ITER_UNIT)
+			if (fractal(p) < (p->iter + ITER_UNIT) / 2)
 			{
 				p->div[x][y] = 1;
 				offset = y * p->line_size + (x * p->bits_per_pixel / 8);
-				*(int *)(p->addr + offset) = 0x00ffffff;
+				*(int *)(p->addr + offset) = 0x00000000;
+			}
+			else if (fractal(p) < (p->iter + ITER_UNIT) * 2 / 3)
+			{
+				p->div[x][y] = 1;
+				offset = y * p->line_size + (x * p->bits_per_pixel / 8);
+				*(int *)(p->addr + offset) = 0x00808080;
+			}
+			else if (fractal(p) < (p->iter + ITER_UNIT) * 5 / 6)
+			{
+				p->div[x][y] = 1;
+				offset = y * p->line_size + (x * p->bits_per_pixel / 8);
+				*(int *)(p->addr + offset) = 0x00c0c0c0;
+			}
+			else if (fractal(p) < p->iter + ITER_UNIT)
+			{
+				p->div[x][y] = 1;
+				offset = y * p->line_size + (x * p->bits_per_pixel / 8);
+				*(int *)(p->addr + offset) = 0x00f5f5f5;
 			}
 			else
 			{
 				offset = y * p->line_size + (x * p->bits_per_pixel / 8);
-				*(int *)(p->addr + offset) = 0x00ff0000;
+				*(int *)(p->addr + offset) = (128 << 24) | 0x00ff0000;
 			}
 		}
 	}
